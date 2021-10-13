9562068c839f494f25dc4235bebffa8723ae9c0b
Séverin Robert <sevrobe21@gmail.com>
2021-10-13 10:01:08 +0200

    initialisation du projet

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void) {
+    // Création variables globales
+
+    // Essai 1
+    printf("\t\nEssai 1");
+    // Essai 2
+    printf("\t\nEssai 2");
+    // Essai 3
+    printf("\t\nEssai 3");
+    // Qualif 1
+    printf("\t\nQualif 1");
+    // Qualif 2
+    printf("\t\nQualif 2");
+    // Qualif 3
+    printf("\t\nQualif 3");
+    // Course
+    printf("\t\nCourse");
+
+    printf("\t\nFin du programme");
+}
